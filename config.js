3fbaaa390b905c1f6b8a778ce43e712df037600a
JARVIS <68321889+jarvis-v3@users.noreply.github.com>
2024-11-07 08:55:52 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.example.js
+const GITHUB_CONFIG = {
+    owner: 'your-github-username',
+    repo: 'your-repository-name',
+    path: 'data/tukin.csv',
+    branch: 'main',
+    token: 'your-github-token'
+};
+
+export { GITHUB_CONFIG };
